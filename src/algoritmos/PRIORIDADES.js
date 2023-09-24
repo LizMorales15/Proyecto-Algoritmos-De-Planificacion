2293250f08817980d38db196df991391dab96f88
Elizabeth <beth11715@gmail.com>
2023-09-24 09:48:20 -0700

    Avance prioridades, no encuentro el error

--- src/algoritmos/PRIORIDADES.js
+++ src/algoritmos/PRIORIDADES.js
@@ -1,73 +1,81 @@
-export class PRIORIDADES{
+export class PRIORIDADES {
     processes;
     queue;
 
-    constructor(processes){
+    constructor(processes) {
         this.processes = processes;
-        this.queue = []; 
+        this.queue = [];
     }
 
-   
-
-    schedule(){
+    schedule() {
         console.log('hola');
-        // Sort the processes by priority
-        this.processes.sort((a, b) => a.priority - b.priority);
+        /* Sort the processes by priority and arrival time
+            so the ones with same priority gets an order by
+            arrival time
+        */
+        this.processes.sort((a, b) => {
+            if (a.priority === b.priority) {
+                return a.time - b.time;
+            }
+            return a.priority - b.priority;
+        });
 
         let currentTime = 0;
         let previousTR = 0;
 
-        //Choose process with more priority level
-        let priorityIndex=0;
-        for (let i = 1; i < this.processes.length; i++) {
-            if (this.processes[i].priority < this.processes[priorityIndex].priority &&
-                this.processes[i].time <= currentTime) {
-                priorityIndex = i;
+        for (let i = 0; i < this.processes.length; i++) {
+            const currentProcess = this.processes[i];
+
+            // Check if the process has arrived
+            if (currentProcess.time <= currentTime) {
+                // Find other processes with the same priority and arrival time
+                const sameArrivalTimeAndPriorityProcesses = this.processes.filter(process =>
+                    process.time === currentProcess.time && process.priority === currentProcess.priority);
+
+                if (sameArrivalTimeAndPriorityProcesses.length > 1) {
+                    // If there are multiple processes with the same arrival time and priority, choose the one that arrived first
+                    sameArrivalTimeAndPriorityProcesses.sort((a, b) => a.time - b.time);
+                }
+                const chosenProcess = sameArrivalTimeAndPriorityProcesses[0];
+
+                // Update the current time and previous TR
+                chosenProcess.TE = currentTime - chosenProcess.time;
+                if (chosenProcess.TE < 0) {
+                    currentTime = chosenProcess.time;
+                    chosenProcess.TE = 0;
+                }
+                chosenProcess.TR = chosenProcess.burstTime + previousTR;
+                chosenProcess.TP = chosenProcess.TE + chosenProcess.burstTime;
+                currentTime += chosenProcess.burstTime;
+                previousTR = chosenProcess.TR;
+
+                // Add the chosen process to the queue
+                this.queue.push(chosenProcess);
+
+                // Remove the chosen process from the processes array
+                this.processes.splice(this.processes.indexOf(chosenProcess), 1);
+
+                // Reset the loop to re-evaluate the remaining processes
+                i = -1;
             }
         }
-        let process = this.processes[priorityIndex];
-        // Make sure the properties are treated as numbers
-        process.priority = Number(process.priority);
-        process.time = Number(process.time);
-
-        // Calculate TE (waiting time) for each process
-        process.TE = currentTime - process.time;
-        if (process.TE < 0) {
-            currentTime = process.time;
-            process.TE = 0;
-        }
-
-        // Calculate TR (response time) for each process
-        process.TR = process.burstTime + previousTR;
-
-        // Calculate TP (completion time) for each process
-        process.TP = process.TE + process.burstTime;
-
-        // Update the current time and previous TR
-        currentTime += process.burstTime;
-        previousTR = process.TR;
-
-        
 
         // Calculate the average values for TE, TR, and TP
-        const averageTE = this.queue.reduce((sum, process) => sum + Number(process.TE), 0) / this.queue.length;
-        const averageTR = this.queue.reduce((sum, process) => sum + Number(process.TR), 0) / this.queue.length;
-        const averageTP = this.queue.reduce((sum, process) => sum + Number(process.TP), 0) / this.queue.length;
+        const averageTE = this.queue.reduce((sum, process) => sum + process.TE, 0) / this.queue.length;
+        const averageTR = this.queue.reduce((sum, process) => sum + process.TR, 0) / this.queue.length;
+        const averageTP = this.queue.reduce((sum, process) => sum + process.TP, 0) / this.queue.length;
 
-    
         // Set the calculated values as properties of the PRIORIDADES instance
         this.averageTE = averageTE;
         this.averageTR = averageTR;
         this.averageTP = averageTP;
-
-        // Set the queue as the sorted processes
-        this.queue = [...this.processes];
+        
+        this.queue = this.queue.concat(this.processes);
 
         // Display the results in the desired format
         console.log('\tTE\tTR\tTP');
-
-        for (let process of this.processes) {
-        console.log(`${process.name}\t${process.TE}\t${process.TR}\t${process.TP}`);
+        for (let process of this.queue) {
+            console.log(`${process.name}\t${process.TE}\t${process.TR}\t${process.TP}`);
         }
 
         console.log(`PROMEDIO\t${averageTE.toFixed(1)} u.t\t${averageTR.toFixed(1)} u.t\t${averageTP.toFixed(1)} u.t`);
